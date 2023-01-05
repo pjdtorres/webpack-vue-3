368b7279a206dfb10c685c0ff170c06d59edc2aa
pjdtorres <paulo.torres@ipcb.pt>
2023-01-05 12:37:00 +0000

    Primeiro commit

--- webpack.config.js
+++ webpack.config.js
@@ -4,20 +4,5 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
 module.exports = {
-    mode: 'development',
-    entry: {
-      index: './src/index.js',
-      print: './src/print.js',
-    },
-   devtool: 'inline-source-map',
-    plugins: [
-      new HtmlWebpackPlugin({
-        title: 'Development',
-      }),
-    ],
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-      clean: true,
-    },
+    entry: './src/main.js'
   };
